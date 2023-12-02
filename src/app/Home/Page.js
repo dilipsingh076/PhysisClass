feb4d0ec11f91b87179868a171d1e98721068216
Dilip singh <dilipsinghf@gmail.com>
2023-12-02 10:39:57 +0530

    addextra work

---
+++ src/app/Home/Page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Herosection from './HeroSection'
+
+const Page = () => {
+  return (
+    <div>
+      <Herosection/>
+    </div>
+  )
+}
+
+export default Page
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
 import { Button, Stack } from '@mui/material'
 import Image from 'next/image'
-import Herosection from '@/components/HeroSection/Herosection'
+import HeroSection from '@/app/Home/Page'
 
 export default function Home() {
   return (
     <>
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <Herosection/>
+     <HeroSection/>
     </main>
     </>
   )
